a970eac3e60511acc7a7e806087b5645cde8e03a
Spiderman <spiderman04@users.noreply.github.com>
2024-06-21 16:07:36 -0400

    Renamed and cleaned
    renamed

---
+++ mongodb_setup_crud.py
@@ -0,0 +1,34 @@
+
+import random
+from pymongo import *  # or use * including binary BSON
+from bson import *
+
+try:
+    client = MongoClient("mongodb://localhost", 27017)
+    print("Connected successfully!!!")
+except:
+    print("Could not connect to MongoDB")
+
+db = client["TestDB"]
+print("TestDB database is created !")
+
+# Created or Switched to collection names: Bios
+collection = db.Bios
+
+viewer_value = random.randrange(100, 999)
+print("New random viewer integer: " + str(viewer_value))
+
+record = {
+    "id": 100,
+    "name": { "first": "George", "last": "Jones" }
+}
+
+# inserting the data in the database
+collection.insert_one(record)
+
+for doc in collection.find({"id" : "George"}):
+    print(doc)
+# close the connection to MongoDB
+db.close
+print("MongoDB database connection closed.")
+
